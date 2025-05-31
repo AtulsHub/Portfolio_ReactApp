c0b8fffa3ed573050e6710870b7ce68ba90705ad
AtulsHub <atulsdev18@gmail.com>
2025-05-31 16:27:10 +0530

    minor fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/portfoio/',
+  base: '/Portfolio_ReactApp/',
   plugins: [react(),tailwindcss()],
 })
